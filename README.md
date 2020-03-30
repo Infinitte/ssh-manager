958676972a6dd692a606a2a842ac07f382822dfd
lorenzo.ortiz.ext <kabble@SSCC-C516.IFAPA>
2020-03-30 09:52:45 +0200

    Fix Readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ssh-manager
+<h1>About</h1>
 Script that read your .ssh/config and makes a menu to choose what server to connect
+
+<h1>Installing</h1>
+Just download and copy somewhere in your path. For instance:
+```bash
+sudo cp ssh-manager.sh /usr/local/bin/ssh-manager
+sudo chmod +x /usr/local/bin/ssh-manager
+```
+
+<h1>Running</h1>
+You need to have a ssh config file on ~/.ssh/config with some Host lines.
+Just type `ssh-manager` with no arguments.
---
+++ ssh-manager.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo '                         __    _                                   '
+echo '                    _wr""        "-q__                             '
+echo '                 _dP                 9m_                           '
+echo '               _#P                     9#_                         '
+echo '              d#@                       9#m                        '
+echo '             d##                         ###                       '
+echo '            J###                         ###L                      '
+echo '            {###K                       J###K                      '
+echo '            ]####K      ___aaa___      J####F                      '
+echo '        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  '
+echo '     _g##############mZ_         __g##############m_               '
+echo '   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             '
+echo "  a###\"\"          ,Z\"#####@\" '######\"\g          \"\"M##m            "
+echo ' J#@"             0L  "*##     ##@"  J#              *#K           '
+echo ' #"               `#    "_gmwgm_~    dF               `#_          '
+echo '7F                 "#_   ]#####F   _dK                 JE          '
+echo ']                    *m__ ##### __g@"                   F          '
+echo '                       "PJ#####LP"                                 '
+echo " \`                       0######_                      '           "
+echo '                       _0########_                                 '
+echo '     .               _d#####^#####m__              ,               '
+echo '      "*w_________am#####P"   ~9#####mw_________w*"                '  
+echo '          ""9@#####@M""           ""P@#####@M""                    '
+echo ''
+
+
+
+hosts="casa ion_pro ion_pre"
+
+hosts=`cat .ssh/config | grep ^Host | grep -v "*" | awk '{print $2}' | sort`
+select option in $hosts
+do
+	ssh $option
+	exit
+done
